2c73515d63e04f592be044436aa70dd32d6a1968
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-08-09 02:40:41 +0100

    setting heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img style="width:60px;" src="{{ asset('icon.png') }}" alt="{{ config('app.name', 'Laravel') }}">
+                    <img style="width:60px;" src="{{ asset('Icon.png') }}" alt="{{ config('app.name', 'Laravel') }}">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
