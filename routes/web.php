23c8cff12948be38467973430ac8f4f53d7f9e10
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-08-12 04:04:46 +0100

    finishing enatounsi

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('featuring', 'App\Http\Controllers\featuring@show');
 Route::get('EnaTounsi', [App\Http\Controllers\ena_tounsi::class , 'show'])->name('ena_tounsi');
 Route::get('ena_tounsi_state_offices', 'App\Http\Controllers\ena_tounsi@showoffices')->name('ena_tounsi_state_offices');
 Route::post('EnaTounsi', [App\Http\Controllers\ena_tounsi::class , 'submit'])->name('ena_tounsi');
-Route::view('success','layouts\success', ["success_message" => "The form has been Successfully submitted!!"])->name('ena_tounsi_success');
+Route::view('success','/layouts/success', ["success_message" => "The form has been Successfully submitted!!"])->name('ena_tounsi_success');
 
 Auth::routes();
 
