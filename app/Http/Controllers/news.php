d0c74435c88b6bf7ba425dc9e43130f4eb84693d
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-08-19 16:18:15 +0100

    small patch

--- app/Http/Controllers/news.php
+++ app/Http/Controllers/news.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Auth;
-use App\Models\user;
+use App\Models\User;
 use App\Models\news as newsmodel;
 
 class news extends Controller
